824465940b221706ac39ae9283217f0bafb743f6
Thibault Clérice <leponteineptique@gmail.com>
2022-07-06 19:39:19 +0200

    0.0.1b - Hotfix for Serialization

--- yaltai/yolo_adapter.py
+++ yaltai/yolo_adapter.py
@@ -19,20 +19,22 @@ def segment(model: str, device: str, input: str) -> Dict[str, List[List[int]]]:
         name: []
         for name in names
     }
-    for i, (im, pred) in enumerate(zip(prediction.imgs, prediction.xyxyn)):
-        img_width, img_height = im.shape[:2]
+    for i, (im, pred) in enumerate(zip(prediction.imgs, prediction.pred)):
+        # img_width, img_height = im.shape[:2]
 
         if not pred.shape[0]:
             return {}
 
-        for x0, y0, x1, y1, conf, cls in reversed(pred):
+        for *box, conf, cls in reversed(pred):
             cls_name = names[int(cls)]
             # Top Left
-            x0 = int(float(x0) * img_width)
-            x1 = int(float(x1) * img_width)
+            #xc = int(xc.item() * img_width)
+            #yc = int(yc.item() * img_height)
             # Bottom Right
-            y0 = int(float(y0) * img_height)
-            y1 = int(float(y1) * img_height)
+            #w = int(w.item() * img_width)
+            #h = int(h.item() * img_height)
+            box = [int(z.item()) for z in box]
+            x0, y0, x1, y1 = box
 
             points = [[x0, y0], [x1, y0], [x1, y1], [x0, y1], [x0, y0]]
             out[cls_name].append(points)
